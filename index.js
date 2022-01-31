db822f0364e18bae264b8b219a27f1843e5c78c7
Brian Vu <brianvu7@gmail.com>
2022-01-31 10:15:14 -0600

    commit tree 42a8413519feeb656593ee2557c8230ac777180b

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Brian";
+let currentUserExcited = currentUser.toUpperCase();
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUserExcited}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
